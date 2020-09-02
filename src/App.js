7477b819dcbed18a9df7f66c00d2e55111e320d5
mannan0819 <mannan1994@hotmail.com>
2020-09-02 15:05:58 -0400

    deloying app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Game from './games';
 import './App.css';
+import './stylesheet.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App borderR">
+        <div className= "main-title">Memory Game</div>
+        <div className=" borderC"> 
+
+          <Game tiles={30} />
+
+        </div>
     </div>
   );
 }
